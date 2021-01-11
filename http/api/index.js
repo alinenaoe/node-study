4638f968b30d31df7ca1726c24a859ed0146b5df
Aline Naoe <alinenaoe@gmail.com>
2021-01-11 15:14:03 -0300

    add api

---
+++ http/api/index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const URL = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const data = require('./urls.json');
+
+function writeFile(callback) {
+    fs.writeFile(
+        path.join(__dirname, 'urls.json'), 
+        JSON.stringify(data, null, 2),
+        err => {
+            if(err) throw err
+            callback(JSON.stringify({ message: "ok" }));
+        }
+    );
+}
+
+http.createServer((request, response) => {
+    const { name, url, del } = URL.parse(request.url, true).query;
+
+    response.writeHead(200, {'Access-Control-Allow-Origin': '*'});
+
+    if(!name || !url) 
+        return response.end(JSON.stringify(data));
+
+    if(del) {
+        data.urls = data.urls.filter(item => String(item.url) !== String(url));
+        return writeFile(message => response.end(message))
+    }
+
+    data.urls.push({name, url});    
+    return writeFile(message => response.end(message))
+    
+}).listen(3000, () => console.log("API is running on port 3000"))
